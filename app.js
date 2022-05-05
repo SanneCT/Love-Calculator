34786d36545951c1937a73ec818e39e728701207
SanneCT <sannect@outlook.com>
2022-05-05 09:14:35 +0200

    update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, function(){
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000 tadaaaaaaa")
 });
